8328c03be5c240dce2ffae33b08c5c65c757ddd3
User <User@DESKTOP-TUF1964>
2024-04-16 18:05:09 +0200

    Ajoutez des fichiers projet.

---
+++ carte.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <Windows.h> // utiliser pour gérer l'encodage
+using namespace std;
+
+int main()
+{
+    SetConsoleOutputCP(65001);
+    cout << "Héllo World" << endl;
+    return 0;
+}
