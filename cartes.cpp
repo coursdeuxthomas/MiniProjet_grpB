151e09d82f836696d5baae21a75d601b1c0ef652
Thomas BOURU <ts.bouru@gmail.com>
2024-04-17 01:05:24 +0200

    Création de la première classe CarteDeRevision

---
+++ cartes.cpp
@@ -0,0 +1,11 @@
+﻿#include "cartes.h"
+#include <iostream>
+using namespace std;
+
+void CarteDeRevision::setRecto(const string& recto) {
+    this->recto_ = recto; // Définit le contenu du côté recto de la carte avec la valeur fournie
+}
+
+void CarteDeRevision::setVerso(const std::string& verso) {
+    this->verso_ = verso; // Définit le contenu du côté verso de la carte avec la valeur fournie
+}
---
+++ cartes.h
@@ -0,0 +1,25 @@
+﻿#ifndef CARTES_H
+#define CARTES_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+class CarteDeRevision {
+private:
+    string recto_;
+    string verso_;
+
+public:
+    // Constructeur
+    CarteDeRevision(const string recto, const string verso) {recto_, verso_ = recto, verso;}
+
+    const string getRecto() const { return recto_; }
+    void setRecto(const string& recto);
+    const string getVerso() const { return verso_; }
+    void setVerso(const string& verso);
+}; //THOMAS.B
+
+
+
+#endif // CARTES_H
+
